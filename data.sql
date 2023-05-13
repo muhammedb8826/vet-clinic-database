55e3cb1253e20b812c0489f16c1602076dfab628
Muhammed Berisso <muhammedb8826@gmail.com>
2023-05-13 08:27:32 +0300

    Add the insertion to species into data.sql

--- data.sql
+++ data.sql
@@ -23,6 +23,9 @@ INSERT INTO owners (full_name,age) VALUES ('Melody Pond', 77);
 INSERT INTO owners (full_name,age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners (full_name,age) VALUES ('Jodie Whittaker', 38);
 
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+
 UPDATE animals SET species_id=2 WHERE name LIKE '%mon';
 UPDATE animals SET species_id=1 WHERE name NOT LIKE '%mon';
 
