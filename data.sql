52771258df53093a82418f590b6181619105d2c1
Muhammed Berisso <muhammedb8826@gmail.com>
2023-05-13 14:22:25 +0300

    Insert data into vets table

--- data.sql
+++ data.sql
@@ -34,3 +34,10 @@ UPDATE animals SET owner_id=2 WHERE name IN ('Gabumon','Pikachu');
 UPDATE animals SET owner_id=3 WHERE name IN ('Devimon','Plantmon');
 UPDATE animals SET owner_id=4 WHERE name IN ('Charmander','Squirtle', 'Blossom');
 UPDATE animals SET owner_id=5 WHERE name IN ('Angemon','Boarmon');
+
+/* Project 4 */
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, 'Apr-23-2000');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, 'Jan-17-2019');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, 'May-04-1981');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, 'Jun-08-2008');
